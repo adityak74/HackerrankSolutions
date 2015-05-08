5d630409e41147166bb3431616a64febcfda7879
Aditya K G <akarnam37@gmail.com>
2015-05-08 19:59:04 +0530

    Updated

--- lonelyint.c
+++ lonelyint.c
@@ -1,33 +1,40 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+int status[101] = {0};
 
-int lonelyinteger(int size, int* a) {
-    
-}
 int main() {
 
     int res;
     int size, i=0;
+    int a[100];
     int item;
     scanf("%d", &size);
 
-    if(size < 100 && size >=1 && (size%2==0)){
-        int a[size];
+    if(size < 100 && size > 0 && !(size%2==0)){
+        
         while(i<size) { 
             
             scanf("%d", &item);
             if(item>=0 && item <=100){
                 a[i] = item;
                 i++;
-                res = lonelyinteger(size, a);
-                printf("%d", res);
-            }
-            else{
-
             }
 
         }
+
+        int j;
+
+                for (j = 0; j < size; j++)
+                {
+                    status[ a[j] ]++;
+                }
+                for (j = 0; j < 101; j++)
+                {
+                    if(status[j]==1){ res = j; }
+                }
+
+                printf("%d", res);
         
         
     }
